b3de85f1af71bf7a2072c859a92bf1d3621277af
brightonkbgrow <brightongrow@byui.edu>
2025-03-14 02:27:36 -0600

    build and push UVU page

---
+++ UtahValleyUniversity/UtahValleyUniversity/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UtahValleyUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using UtahValleyUniversity.Models.SchoolViewModels;
+using UtahValleyUniversity.Data;
+
+namespace UtahValleyUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SchoolContext _context;
+
+        public HomeController(ILogger<HomeController> logger, SchoolContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> About()
+        {
+            var data = await _context.Students
+                .GroupBy(s => s.EnrollmentDate)
+                .Select(g => new EnrollmentDateGroup
+                {
+                    EnrollmentDate = g.Key,
+                    StudentCount = g.Count()
+                })
+                .ToListAsync();
+
+            return View(data);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UtahValleyUniversity/UtahValleyUniversity/Migrations/20250314061432_RemoveCourseTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UtahValleyUniversity.Migrations
+{
+    public partial class RemoveCourseTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
